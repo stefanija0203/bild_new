ba1232294cae03204aec217a85c302db31c9e676
Stefanija Kicovic <stefanijakicovic@gmail.com>
2015-02-13 18:33:54 +0100

    Package page - table section.

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -5,3 +5,19 @@ $(function() {
     timeout: 3000,
   });
 });
+
+$(function() {
+  $("td").mouseover(function() {
+    var targetIndex, elements;
+    targetIndex = $(this).index() + 1;
+    elements = $("td");
+    elements.filter(":nth-child(" + targetIndex + ")").css("background-color", "#eff9fe");
+    elements.not(":nth-child(" + targetIndex + ")").css("background-color", "transparent");
+  });
+  $("table").mouseleave(function() {
+    $("td").css("background-color", "transparent");
+  });
+  $("tr:last-child td").mouseover(function() {
+    $("td").css("background-color", "transparent");
+  });
+});
